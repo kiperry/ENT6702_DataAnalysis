5e9d629676343a90b15e7dd28233a0c96cf73386
Kayla I Perry <kiperry1488@gmail.com>
2023-11-21 12:54:44 -0500

    week 13 materials

---
+++ Week 13/Ant community example/Ants_Multivariate.R
@@ -0,0 +1,11 @@
+############################################################################
+# ENT6702 Entomological Techniques and Data Analysis
+#
+# Multivariate analyses
+#
+# National Science Foundation (NSF)
+# Long-term Ecological Research (LTER)
+# Harvard Forest
+#
+# 16 November 2023
+############################################################################
